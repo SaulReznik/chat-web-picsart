8e474d76163ddae3881207adefbc96e1ddc64cf1
SaulReznik <robhov97@gmail.com>
2020-11-27 18:36:02 +0400

    wss

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -15,7 +15,7 @@ const Chat = () => {
   const [isConnected, setIsConnected] = useState(false);
 
   const socketRef = useRef(
-    io('https://chat-socket-picsart.herokuapp.com', {
+    io('wss://chat-socket-picsart.herokuapp.com', {
       query: { token }
     })
   );
